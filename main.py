7f0c2aae77857d01f858123e9b09fba45d876908
Alessandro Biz <alessanbiz@gmail.com>
2022-03-15 22:12:42 -0300

    threads calculator and mat plot

--- main.py
+++ main.py
@@ -1,22 +1,73 @@
 from time import perf_counter_ns
 import simples as sp
 import mtrhead as mt
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+i = 0
+sumSimple = 0
+nSample = 2
+nPrimoSimple = []
+nPrimoThread = []
+nThread = 3
 
 with open("data.csv") as file:
     data = [line.strip() for line in file]
 
 data = list(map(int, data))
 
+
 print('\n\nanalise de %d valores\n\n'%(len(data)))
-start1 = perf_counter_ns()
-primo_sp = sp.resolve_simples(data)
-finish1 = perf_counter_ns()
 
-start2 = perf_counter_ns()
-primo_mt = mt.resolve_trhread(data)
-finish2 = perf_counter_ns()
 
-print('simples          > threads')
-print('%f ms   > %f ms  : tempo execucao'%((finish1-start1)/1000000,(finish2-start2)/1000000))
-print('%d            > %d           :numeros primos encontrados'%(primo_sp,primo_mt))
+
+while i < nSample:
+    start1 = perf_counter_ns()
+    primo_sp = sp.resolve_simples(data)
+    finish1 = perf_counter_ns()
+
+    sumSimple += (finish1-start1)/1000000
+    nPrimoSimple.append(primo_sp)
+    i+=1
+
+
+
+arThread = []
+
+for j in range(1 , nThread + 1):
+
+    sumThread = 0
+    k = 0
+    while k < nSample:
+
+        start2 = perf_counter_ns()
+        primo_mt = mt.resolve_thread(data, j)
+        finish2 = perf_counter_ns()
+
+        nPrimoThread.append(primo_mt)
+        sumThread+= (finish2-start2)/1000000
+
+        k+= 1
+
+    arThread.append(sumThread / nSample)
+
+print('Simple: %f ms' %(sumSimple / nSample))
+
+for l in range(len(arThread)):
+    print('Thread: %f ms' %arThread[l])
+
+
+print('Array primo simples', nPrimoSimple)
+print('Array primo thread', nPrimoThread)
+
+horizontalLineThread = np.arange(1, nThread + 1, 1)
+
+plt.title("THREAD")
+plt.plot(horizontalLineThread, arThread, )
+
+plt.xlabel("Threads")
+plt.ylabel("Tempo (ms)")
+plt.show()
 
--- mtrhead.py
+++ mtrhead.py
@@ -9,16 +9,35 @@ def tCalculaPrimo(data):
             primos += 1
     return primos
 
-def resolve_trhread(data):
-    ThreadsQtdd = 5
+
+def resolve_thread(data, nThread):
+    ThreadsQtdd = nThread
     tamanholista = len(data)
-    index = range(0, tamanholista+(tamanholista//ThreadsQtdd), tamanholista//ThreadsQtdd)
+
+    index = range(0, tamanholista + (tamanholista // ThreadsQtdd), tamanholista // ThreadsQtdd)
+
+    print('nThread ', nThread, ' = ', index)
+
+    for i in range(len(index) - 1):
+        print(i, '=', index[i], ' até ', index[i+1])
+
+    print("")
     primos = 0
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
         for i in range(ThreadsQtdd):
-            futures.append(executor.submit(tCalculaPrimo, data=data[index[i]:index[i+1]]))
+
+            if ThreadsQtdd - 1 == i and index[i+1] < tamanholista:
+                lastIndex = tamanholista
+            else:
+                lastIndex = index[i+1]
+
+            futures.append(executor.submit(tCalculaPrimo, data=data[index[i]:lastIndex]))
         for future in concurrent.futures.as_completed(futures):
-            #futures.append(future.result())
+            # futures.append(future.result())
             primos += future.result()
     return primos
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
